4f3a410ddee897b4bc4d1109aa578ba273b36406
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-07-26 14:54:15 +0100

    added helper for comment creation and some feature tests

--- spec/features/users_can_comment_on_posts_spec.rb
+++ spec/features/users_can_comment_on_posts_spec.rb
@@ -3,8 +3,23 @@ require 'pry'
 
 RSpec.feature "Comment section is visible", type: :feature, js: true do
   scenario "click on comment within post and see the empty comment thread" do
-    seed_post
-    sign_up
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "giraffe@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Submit"
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, giraffe!"
+    click_button "Submit"
+    click_link "Logout"
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "tesymcemail@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Submit"
     find_link("Comment").click
     expect(page).to have_content "Hello, giraffe!"
     expect(page).to have_css '.comment-thread'
@@ -12,9 +27,28 @@ RSpec.feature "Comment section is visible", type: :feature, js: true do
   end
 
   scenario "Two commenters post two comments" do
-    post_comment
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "opinionatedtroll@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Submit"
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "No!"
+    click_button "Submit"
+    click_link "Comment"
+    find_link("Show Comments (0)", visible: false).click
+    find_link("New Comment", visible: false).click
+    find("textarea#comment_body", visible: false).set("I disagree.")
+    find_button("Post Comment").click
     click_link "Logout"
-    sign_up
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "tesymcemail@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Submit"
     find_link("Comment").click
     find_link("Show Comments (1)", visible: false).click
     find_link("New Comment", visible: false).click
@@ -22,7 +56,7 @@ RSpec.feature "Comment section is visible", type: :feature, js: true do
     find_button("Post Comment").click
     expect(page).to have_content("I disagree.")
     expect(page).to have_content("Hell no.")
-    expect(page).to have_css('.comment', text: "Hell no." )
+    expect(page).to have_css('.comment', text: "Hell no.")
     expect(page).to have_css('.comment_commontator', text: "tesymcemail@email.com")
     expect(page).to have_css('.comment_commontator', text: "opinionatedtroll@email.com")
   end
