8982a9a9699dcf67fa78357deafb0cf378c16822
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-07-26 14:35:59 +0100

    test: stub authenticate_user! in chat controller spec

--- spec/controllers/chat_controller_spec.rb
+++ spec/controllers/chat_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ChatController, type: :controller do
+  before(:each) do
+    allow(controller).to receive(:authenticate_user!).and_return(true)
+  end
 
   describe "GET #show" do
     it "returns http success" do
